c1d390176a64f41967395738b036170f96985f60
youthloser666 <androdev909@gmail.com>
2025-06-12 15:30:27 +0700

    Implement code changes to enhance functionality and improve performance

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 document.addEventListener('contextmenu', function(e) {
   e.preventDefault(); // Mencegah tindakan default dari klik kanan
 });
+
+
+  const img = document.getElementById('profile-img');
+  const staticSrc = 'img/pp.png';
+  const gifSrc = 'img/uwu.gif';
+
+  img.addEventListener('mouseenter', () => {
+    img.src = gifSrc;
+  });
+  img.addEventListener('mouseleave', () => {
+    img.src = staticSrc;
+  });
